df80e97bf9e7ec3df623ac0729e5b51265895b7e
vickylz <victorialopezzamora16@gmail.com>
2019-11-20 22:31:42 -0300

    Initial commit

---
+++ archivo.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("boton");
+boton.addEventListener('click', (e) => {
+        setTimeout(() => {
+                alert("Tu mensaje fue enviado, me contactaré contigo a la brevedad.");
+        })
+    });
